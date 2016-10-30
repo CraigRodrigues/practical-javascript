3304587e95a533c6ca1b17b8b32e824c5792c34b
Craig Rodrigues <PagingCraig@gmail.com>
2016-10-30 18:17:57 -0400

    Created version 5 and completed first requirement

---
+++ version5.js
@@ -0,0 +1,38 @@
+// PSJ - V5
+
+// V5 Requirements
+
+// .displayTodos should show .todoText
+// .displayTodos should tell you if .todos is empty
+// .displayTodos should show .completed
+
+var todoList = {
+    todos: [],
+    displayTodos: function() {
+        console.log("My Todos:");
+        for (var i = 0; i < this.todos.length; i++) {
+            console.log(this.todos[i].todoText);
+        }
+    },
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    changeTodo: function(position, todoText) {
+        var todo = this.todos[position];
+        todo.todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position,1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }
+};
