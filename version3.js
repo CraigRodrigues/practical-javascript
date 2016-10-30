3b9331fbc233f3586dc8d651d163477e9967fc38
Craig Rodrigues <PagingCraig@gmail.com>
2016-10-30 10:16:29 -0400

    Finished: It should have an addTodo method.

--- version3.js
+++ version3.js
@@ -11,5 +11,9 @@ var todoList = {
     todos: ["item1", "item2", "item3"],
     displayTodos: function() {
         console.log("My Todos:", this.todos);
+    },
+    addTodo: function(todo) {
+        this.todos.push(todo);
+        this.displayTodos();
     }
 };
