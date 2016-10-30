a5af17666fedebfae4c9b81c822509f08930709b
Craig Rodrigues <PagingCraig@gmail.com>
2016-10-30 09:59:39 -0400

    Setup v3 js file

---
+++ version3.js
@@ -0,0 +1,10 @@
+// PSJ - V3
+
+// V3 Requirements
+// It should store the todos array on an object
+// It should have a displayTodos method
+// It should have an addTodo method
+// It should have a changeTodo method
+// It should have a deleteTodo method
+
+console.log("Hello world!");
