cdeab9bb102ab05fdbf99d7e7a2b8c4250c372f1
Craig Rodrigues <PagingCraig@gmail.com>
2016-11-02 15:01:12 -0400

    Version 9: Escaped from the console! Cleaned up code to remove all of the console logging displayTodo functions. Also added view.displayTodo method to all button clicks

--- app.js
+++ app.js
@@ -8,40 +8,22 @@
 
 var todoList = {
     todos: [],
-    displayTodos: function () {
-        if (this.todos.length === 0) {
-            console.log("Your Todo List is Empty!");
-        } else {
-            console.log("My Todos:");
-            for (var i = 0; i < this.todos.length; i++) {
-                if (this.todos[i].completed === true) {
-                    console.log("[X]", this.todos[i].todoText);   
-                } else {
-                    console.log("[ ]", this.todos[i].todoText);
-                }
-            }
-        }
-    },
     addTodo: function(todoText) {
         this.todos.push({
             todoText: todoText,
             completed: false
         });
-        this.displayTodos();
     },
     changeTodo: function(position, todoText) {
         var todo = this.todos[position];
         todo.todoText = todoText;
-        this.displayTodos();
     },
     deleteTodo: function(position) {
         this.todos.splice(position,1);
-        this.displayTodos();
     },
     toggleCompleted: function(position) {
         var todo = this.todos[position];
         todo.completed = !todo.completed;
-        this.displayTodos();
     },
     toggleAll: function() {
         var totalTodos = this.todos.length;
@@ -61,19 +43,16 @@ var todoList = {
                 this.todos[i].completed = true;
             }
         }
-        this.displayTodos();
     }
 };
 
 // handles buttons, click events, and user input
 var handlers = {
-    displayTodos: function () {
-        todoList.displayTodos();
-    },
     addTodo: function () {
         var addTodoInputText = document.getElementById("addTodoInputText");
         todoList.addTodo(addTodoInputText.value);
         addTodoInputText.value = "";
+        view.displayTodos();
     },
     changeTodo: function () {
         var changeTodoInputPosition = document.getElementById("changeTodoInputPosition");
@@ -83,23 +62,28 @@ var handlers = {
         
         changeTodoInputPosition.value = "";
         changeTodoInputText.value = "";
+        
+        view.displayTodos();
     },
     deleteTodo: function () {
         var deleteTodoInputPosition = document.getElementById("deleteTodoInputPosition");
         todoList.deleteTodo(deleteTodoInputPosition.valueAsNumber);
         deleteTodoInputPosition.value = "";
+        view.displayTodos();
     },
     toggleCompleted: function () {
         var toggleCompletedInputPosition = document.getElementById("toggleCompletedInputPosition");
         todoList.toggleCompleted(toggleCompletedInputPosition.valueAsNumber);
         toggleCompletedInputPosition.value = "";
+        view.displayTodos();
     },
     toggleAll: function () {
         todoList.toggleAll();
+        view.displayTodos();
     }
 };
 
-// controls what the user sees
+// only controls what the user sees and nothing else
 var view =  {
     displayTodos: function () {
         var todosUl = document.querySelector("ul");
