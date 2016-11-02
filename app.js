dbcff5f02f938423a81e6792c3aa34a2c2bf52ed
Craig Rodrigues <PagingCraig@gmail.com>
2016-11-02 11:36:55 -0400

    Updated reqs for version 8. Changed to one file app.js and version will now all be under Git because making new files for each version makes no damn sense

---
+++ app.js
@@ -0,0 +1,78 @@
+// PSJ - V8
+
+// V8 Requirements
+
+// Add working controls for .addTodo
+// Add working controls for .changeTodo
+// Add working controls for .deleteTodo
+// Add working controls for .toggleCompleted
+
+var todoList = {
+    todos: [],
+    displayTodos: function() {
+        if (this.todos.length === 0) {
+            console.log("Your Todo List is Empty!");
+        } else {
+            console.log("My Todos:");
+            for (var i = 0; i < this.todos.length; i++) {
+                if (this.todos[i].completed === true) {
+                    console.log("[X]", this.todos[i].todoText);   
+                } else {
+                    console.log("[ ]", this.todos[i].todoText);
+                }
+            }
+        }
+    },
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    changeTodo: function(position, todoText) {
+        var todo = this.todos[position];
+        todo.todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position,1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    },
+    toggleAll: function() {
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+        for (var i = 0; i < this.todos.length; i++) {
+            if (this.todos[i].completed === true) {
+                completedTodos++;
+            }
+        }
+        
+        if (completedTodos === totalTodos) {
+            for (var i = 0; i < this.todos.length; i++) {
+                this.todos[i].completed = false;
+            }
+        } else {
+            for (var i = 0; i < this.todos.length; i++) {
+                this.todos[i].completed = true;
+            }
+        }
+        this.displayTodos();
+    }
+};
+
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+
+displayTodosButton.addEventListener('click', function() {
+    todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function() {
+   todoList.toggleAll(); 
+});
